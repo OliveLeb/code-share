b713fec9c439dd42c2c64e11f4cd678218dfbd83
OliveLeb <lebel.olivier@wanadoo.fr>
2020-11-12 19:04:21 +0100

    removing codemirror, line count, css

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,71 @@
 window.addEventListener('load', ()=> {
 
-    var editor = CodeMirror.fromTextArea(
-        document.querySelector('#textarea'),{
-            lineNumbers: true,
-            mode:'xml',
-            theme:'darcula',
-            autoCloseTags: true,
-            autoCloseBrackets: true,
-            scrollbarStyle: 'simple'
-        }
-    );    
+const editor = document.querySelector('#editor');
+const lineNumber = document.querySelector('#lineNumber');
 
-    const socket = io();
-
-    const textarea = document.querySelector('#textarea');textarea.value
+    const socket = io();  
     const room =  location.pathname;
     
     socket.on('connect', ()=> {
-
         socket.emit('joinRoom', room);
 
         socket.on('new user', (data)=> {
-             if(data === 'new') socket.emit('message new user', {message:textarea.value,room});
+             if(data === 'new') socket.emit('message new user', {message:editor.value,room});
         });
 
-        textarea.addEventListener('input', ()=> {
-            socket.emit('message', {message:textarea.value,room});
-        });
-
+        editor.addEventListener('input',()=>{
+            socket.emit('message', {message:editor.value,room});
+        })
+        
         socket.on('newMessage', (message)=> {
-            textarea.value = message;
+            editor.value = message;
+            countLine();
         });
-            
+    });
 
+
+
+    editor.addEventListener('scroll', ()=> {
+        lineNumber.scrollTop = editor.scrollTop;
+    });
+    lineNumber.addEventListener('scroll',()=> {
+        editor.scrollTop = lineNumber.scrollTop;
     })
 
+function countLine(){
+    let linesCount = editor.value.split('\n');
+
+    //if(message.split('\n').length !== linesCount.length) return;
+
+    while(lineNumber.firstChild){
+        lineNumber.removeChild(lineNumber.firstChild);
+    };
+
+    linesCount.forEach((line,index) => {
+        let number = document.createElement('p');
+        number.innerText = index + 1;
+        lineNumber.appendChild(number);
+    });
+};
+
+
+
+function download(filename,text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+};
+
+const downloadBtn = document.querySelector('#download');
+downloadBtn.addEventListener('click',()=>{
+    download('codeshare.txt',editor.value);
+});
 
 });
